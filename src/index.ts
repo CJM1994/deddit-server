fb5f531d7ed1c2c52f1ddf7cea48cdc5e95c9b09
CJM1994 <connorjohnmullin@gmail.com>
2022-05-07 18:33:00 -0700

    install mikroorm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import {MikroORM} from '@mikro-orm/core'
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    type: 'postgresql',
+    dbName: 'deddit',
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    debug: process.env.NODE_ENV !== 'production',
+  });
+}
+
+main();
