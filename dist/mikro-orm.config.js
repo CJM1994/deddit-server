22a7da277ac1d3a6f26ac97f85bb7c0cbb02bdd6
CJM1994 <connorjohnmullin@gmail.com>
2022-05-08 01:42:08 -0700

    install mikro-orm cli and migrations, make first migration

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -1,8 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Post_1 = require("./entities/Post");
 const constants_1 = require("./constants");
+const path_1 = __importDefault(require("path"));
 const config = {
+    migrations: {
+        path: path_1.default.join(__dirname, './migrations'),
+        glob: '!(*.d).{js,ts}', // how to match migration files (all .js and .ts files, but not .d.ts)
+    },
     entities: [Post_1.Post],
     type: 'postgresql',
     dbName: 'deddit',
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { Options } from '@mikro-orm/core'
 import { Post } from './entities/Post'
 import { __prod__ } from './constants'
+import path from 'path'
 
 const config: Options = {
+  migrations: {
+    path: path.join(__dirname, './migrations'), // path to the folder with migrations
+    glob: '!(*.d).{js,ts}', // how to match migration files (all .js and .ts files, but not .d.ts)
+  },
   entities: [Post],
   type: 'postgresql',
   dbName: 'deddit',
