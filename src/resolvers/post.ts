b39cbb5c3d2880e8bf981bc37a88d78071af3c7c
CJM1994 <connorjohnmullin@gmail.com>
2022-05-08 22:39:27 -0700

    add query for single posts and mutation to create new posts when given a title

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,12 +1,30 @@
-import { Resolver, Query, Ctx } from 'type-graphql'
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql'
 import { Post } from '../entities/Post'
 import { MainContext } from '../types'
 
 @Resolver()
 
 export class PostResolver {
+
   @Query(() => [Post])
   posts(@Ctx() { em }: MainContext): Promise<Post[]> {
-    return em.find(Post, {}); 
+    return em.find(Post, {});
   }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MainContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title', () => String) title: string,
+    @Ctx() { em }: MainContext): Promise<Post | null> {
+      const post = em.create(Post, { title });
+      await em.persistAndFlush(post);
+      return post;
+  }
+
 }
