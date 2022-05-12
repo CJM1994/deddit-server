aeb4e67cc45b7e710d2d7bf3ba73230ea03e8a46
CJM1994 <connorjohnmullin@gmail.com>
2022-05-11 18:31:41 -0700

    install redis and session storage for cookies

--- dist/index.js
+++ dist/index.js
@@ -12,23 +12,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@mikro-orm/core");
-const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+// Express & Apollo Server
 const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
+// Mikro-ORM & GraphQL
+const core_1 = require("@mikro-orm/core");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const type_graphql_1 = require("type-graphql");
 const hello_1 = require("./resolvers/hello");
 const post_1 = require("./resolvers/post");
 const user_1 = require("./resolvers/user");
 require("reflect-metadata");
+// Session storage
+const express_session_1 = __importDefault(require("express-session"));
+const redis_1 = require("redis");
+const connect_redis_1 = __importDefault(require("connect-redis"));
+const constants_1 = require("./constants");
 const bootServer = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const app = (0, express_1.default)();
+    const app = express_1.default();
+    // Session storage (cookies / user auth)
+    // Must come before apollo-server middleware
+    const RedisStore = connect_redis_1.default(express_session_1.default);
+    const redisClient = redis_1.createClient({ legacyMode: true });
+    redisClient.connect().catch(console.error);
+    app.use(express_session_1.default({
+        name: 'qid',
+        store: new RedisStore({
+            client: redisClient,
+            disableTouch: true,
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365,
+            httpOnly: true,
+            secure: constants_1.__prod__,
+        },
+        saveUninitialized: false,
+        secret: "lkjads?flkasdhDlkahsdflkjadsh",
+        resave: false,
+    }));
     const httpServer = http_1.default.createServer(app);
     const server = new apollo_server_express_1.ApolloServer({
-        schema: yield (0, type_graphql_1.buildSchema)({
+        schema: yield type_graphql_1.buildSchema({
             resolvers: [hello_1.HelloResolver, post_1.PostResolver, user_1.UserResolver],
             validate: false,
         }),
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,54 @@
-import { MikroORM } from '@mikro-orm/core'
-import config from './mikro-orm.config'
-
+// Express & Apollo Server
 import http from 'http'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 
+// Mikro-ORM & GraphQL
+import { MikroORM } from '@mikro-orm/core'
+import config from './mikro-orm.config'
 import { buildSchema } from 'type-graphql'
 import { HelloResolver } from './resolvers/hello'
 import { PostResolver } from './resolvers/post'
 import { UserResolver } from './resolvers/user'
 import 'reflect-metadata'
 
+// Session storage
+import session from "express-session"
+import { createClient } from "redis"
+import connectRedis from 'connect-redis'
+
+import { __prod__ } from './constants'
+
 const bootServer = async () => {
   const orm = await MikroORM.init(config);
   await orm.getMigrator().up();
 
   const app = express();
+
+  // Session storage (cookies / user auth)
+  // Must come before apollo-server middleware
+  const RedisStore = connectRedis(session)
+  const redisClient = createClient({ legacyMode: true })
+  redisClient.connect().catch(console.error)
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+        httpOnly: true,
+        secure: __prod__, // when true cookie only works through https
+      },
+      saveUninitialized: false,
+      secret: "lkjads?flkasdhDlkahsdflkjadsh",
+      resave: false,
+    })
+  )
+
   const httpServer = http.createServer(app);
   const server = new ApolloServer({
     schema: await buildSchema({
